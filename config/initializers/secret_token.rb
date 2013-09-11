ac17c866f4ca7aa13cbb0b393f1c9c3bca9f4cc1
marcosteixeira <administrador@MacBook-Marcos.local>
2013-09-10 21:16:52 -0300

    Criação projeto rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LuceneSolr::Application.config.secret_key_base = 'f4e62a81d8538505846e328aee5e6141dc4f6fdf742834056da8f34d18418574d62385e3c95bc219a5b4f1117372ce62fe38ad0a39907558b5d9c23c311dcb22'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LuceneSolr::Application.config.session_store :cookie_store, key: '_Lucene&Solr_session'
